cc3815e8c3ae6a0ed123c0a2c5a1dd01889ae587
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-11-09 12:41:32 +0530

    Adding Meaningfull Comment

--- Linked_List/Basic_Implementation_Of_LL.c
+++ Linked_List/Basic_Implementation_Of_LL.c
@@ -1,20 +1,21 @@
+/*Author :- Aditya Yadav */
 #include<stdio.h>
-#include<stdlib.h>
-struct node
+#include<stdlib.h> //Library To Use Malloc Function Its a Standard Library
+struct node //Basic Structure of Each Node
 {
     int data;
-    struct node *next;
+    struct node *next; //A Pointer Element To Store The Location of Structure like its own
 };
-typedef struct node Node;
-Node *head;
-Node *temp;
-Node *insert(Node *ins,int val)
+typedef struct node Node; //To use a Short Form
+Node *head; //Head Node
+Node *temp; //Temporary Node
+Node *insert(Node *ins,int val) //Inserting New Node Or To Create a fresh Linked List
 {
     Node *new;
-    new=(Node*)malloc(sizeof(Node));
+    new=(Node*)malloc(sizeof(Node)); //Allocating the Size
     new->data=val;
     new->next=NULL;
-    if(ins==NULL)
+    if(ins==NULL) //Checking whether Linked List Exist Or Not
     {
         ins=new;
         temp=new;
@@ -26,16 +27,16 @@ Node *insert(Node *ins,int val)
     }
     return ins;
 }
-void display(Node *dis)
+void display(Node *dis) //Printing the Complete Linked List
 {
-    if(dis==NULL)
+    if(dis==NULL) //Checking whether Linked List Exist Or Not
     {
         printf("List is Empty . \n");
         printf("First Insert Item .");
     }
     else
     {
-        printf("List Element are :- ");
+        printf("List Element are :- "); //If List Exist Then Printing It
         while(dis!=NULL)
         {
             printf("%d ",dis->data);
@@ -47,13 +48,13 @@ void display(Node *dis)
 int main()
 {
     int n,value;
-    printf("Enter the Number Of Nodes :- ");
+    printf("Enter the Number Of Nodes :- "); //Taking The Number of Nodes From user
     scanf("%d",&n);
     while(n-->0)
     {
         printf("Enter The Element :- ");
         scanf("%d",&value);
-        head=insert(head,value);
+        head=insert(head,value); //Adding the Value By Insert Function Declare Above
     }
     display(head);
 }
