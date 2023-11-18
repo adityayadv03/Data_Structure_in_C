923cb395fd941994f2f632184b87f464b083f6b4
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-11-18 10:19:40 +0530

    Sorting in Linked List

---
+++ Linked_List/Sorting_LL.c
@@ -0,0 +1,83 @@
+/*Author :- Aditya Yadav */
+#include<stdio.h>
+#include<stdlib.h> //Library To Use Malloc Function Its a Standard Library
+struct node //Basic Structure of Each Node
+{
+    int data;
+    struct node *next; //A Pointer Element To Store The Location of Structure like its own
+};
+typedef struct node Node; //To use a Short Form
+Node *head; //Head Node
+Node *temp; //Temporary Node
+Node *insert(Node *ins,int val) //Inserting New Node Or To Create a fresh Linked List
+{
+    Node *new;
+    new=(Node*)malloc(sizeof(Node)); //Allocating the Size
+    new->data=val;
+    new->next=NULL;
+    if(ins==NULL) //Checking whether Linked List Exist Or Not
+    {
+        ins=new;
+        temp=new;
+    }
+    else
+    {
+        temp->next=new;
+        temp=new;
+    }
+    return ins;
+}
+void display(Node *dis) //Printing the Complete Linked List
+{
+    if(dis==NULL) //Checking whether Linked List Exist Or Not
+    {
+        printf("List is Empty . \n");
+        printf("First Insert Item .");
+    }
+    else
+    {
+        printf("List Element are :- "); //If List Exist Then Printing It
+        while(dis!=NULL)
+        {
+            printf("%d ",dis->data);
+            dis=dis->next;
+        }
+    }
+    printf("\n");
+}
+Node *sort(Node *srt)
+{
+    int temp;
+    Node *ptr1=srt;
+    Node *ptr2;
+    while(ptr1!=NULL)
+    {
+        ptr2=ptr1->next;
+        while(ptr2!=NULL)
+        {
+            if(ptr1->data>ptr2->data)
+            {
+                temp=ptr1->data;
+                ptr1->data=ptr2->data;
+                ptr2->data=temp;
+            }
+            ptr2=ptr2->next;
+        }
+        ptr1=ptr1->next;
+    }
+    return srt;
+}
+int main()
+{
+    int n,value;
+    printf("Enter the Number Of Nodes :- "); //Taking The Number of Nodes From user
+    scanf("%d",&n);
+    while(n-->0)
+    {
+        printf("Enter The Element :- ");
+        scanf("%d",&value);
+        head=insert(head,value); //Adding the Value By Insert Function Declare Above
+    }
+    head=sort(head);
+    display(head);
+}
