c2553a18a2424c5889691424f69fcbf6857f04dd
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-11-22 21:37:17 +0530

    Insertion At Position

--- Arrays/Insertion_At_Position.c
+++ Arrays/Insertion_At_Position.c
@@ -8,34 +8,34 @@ void display(int ar[],int n) //Function To Display the Array
         printf("%d ",ar[i]);
     }
 }
-void insert(int ar[] , int n)
+void insert(int ar[] , int n) //Function To Insert A Element
 {
-    int el,po;
+    int el,po; //Declaring Variable For Element and Position
     int check=1;
-    printf("Enter the Element To Insert :- ");
+    printf("Enter the Element To Insert :- "); //Taking Input Element 
     scanf("%d",&el);
-    printf("Enter The Index To Insert :- ");
+    printf("Enter The Index To Insert :- "); //Taking Input Position
     scanf("%d",&po);
-    if(po<0 || po>=n)
+    if(po<0 || po>=n)//Checking for Wrong Index
     {
-        printf("Wrong Position .");
+        printf("Wrong Position .\n");
         check=0;
     }
     else
     {
-        for(int i=n-1 ; i>po ; i--)
+        for(int i=n-1 ; i>po ; i--) // Shifting Element By One Position Backward
         {
             ar[i]=ar[i-1];
         }
-        ar[po]=el;
+        ar[po]=el; //Inserting Element to position
     }
     if(check==1)
     {
-        display(ar,n);
+        display(ar,n); //Printing if Element is Inserted
     }
     else
     {
-        display(ar,n-1);
+        display(ar,n-1); //Printing if Element is Not Inserted
     }
 }
 int main()
