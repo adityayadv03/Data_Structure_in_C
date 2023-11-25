b6b727903ce444d659301e9feedf72bd3a95209f
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-11-25 10:39:30 +0530

    Searching In Array

--- Arrays/Searching_In_Array.c
+++ Arrays/Searching_In_Array.c
@@ -8,25 +8,25 @@ void display(int ar[],int n) //Function To Display the Array
         printf("%d ",ar[i]);
     }
 }
-void search(int ar[],int n)
+void search(int ar[],int n) //Function to Search in Array
 {
     int ele,po=-1;
-    printf("Enter The Element To Search :- ");
+    printf("Enter The Element To Search :- "); //Taking Input Of Element To Search
     scanf("%d",&ele);
-    for(int i=0 ; i<n ; i++)
+    for(int i=0 ; i<n ; i++) //Running The Loop To Check Every Element
     {
-        if(ele==ar[i])
+        if(ele==ar[i]) //Checking With Every Element 
         {
-            po=i;
+            po=i; //If the Element id Found Storing its Location
             break;
         }
     }
-    if(po!=-1)
+    if(po!=-1) //If The Position is other than -1 Than Element Found 
     {
         po=po+1;
         printf("Element Found :- %d .",po);
     }
-    else
+    else //If The Position is -1 Then Element Not Found
     {
         printf("Element Not Found . ");
     }
