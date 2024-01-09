c96c9e16d2b60003f10505418e2696c68a7a092d
adityayadv03 <officialuser.aditya2022@gmail.com>
2024-01-09 18:40:30 +0530

    Stack Array Representation

--- Stack/Array_Representation_Stack.c
+++ Stack/Array_Representation_Stack.c
@@ -1,29 +1,29 @@
 /*Author :- Aditya Yadav */
 #include<stdio.h> //Stack is Represented by Array in the Following Program
-#define max 7
-int st[max],top=NULL;
-void push(int value)
+#define max 7 //the Size of max to 7
+int st[max],top=NULL; //Declaring the Stack as Array of Size max and the top variable to NULL
+void push(int value) //Function to insert or push any value in the Stack
 {
-	if(top==max-1)
+	if(top==max-1) //Checking if the Stack is Full or Not If the Condition is True then Displaying the message Overflow
 	{
 		printf("\nOverflow . \n");
 		printf("\n");
 	}
-	else
+	else //If the Condition is False then inserting the Value
 	{
 		top++;
 		st[top]=value;
 	}
 }
-void pop()
+void pop() //Function to Delete or pop any Value From Stack
 {
-	int val;
-	if(top==NULL)
+	int val; //Variable to Store the deleted Value
+	if(top==NULL) //Checking Whether the Stack is Empty or Not if the condition is true then the Stack is Empty
 	{
 		printf("\nUnderflow . \n");
 		printf("\n");
 	}
-	else
+	else // If Condition is False then Poping the Variable
 	{
 		val=st[top];
 		top--;
@@ -31,13 +31,13 @@ void pop()
 		printf("\n");
 	}
 }
-void display()
+void display() //Fucntion to Display All the Element Present in Stack
 {
-	if(top==NULL)
+	if(top==NULL) //Checking Whether the Stack is Empty or Not if the condition is true then the Stack is Empty
 	{
 		printf("Stack is Empty . ");
 	}
-	else
+	else //If Condition is False Then Displaying the Present Element
 	{
 		printf("Stack Elements Are :- ");
 		for(int i=top ; i>0 ; i--)
@@ -50,15 +50,15 @@ void display()
 int main()
 {
 	int value,ch;
-	do
+	do //Creating a Menu Driven Program to Successively perform push or pop operation
 	{
 		printf("1.Push . \n");
 		printf("2.Pop . \n");
 		printf("3.Display . \n");
 		printf("4.Exit .\n");
-		printf("Enter the Choice :- ");
+		printf("Enter the Choice :- "); //Taking Choice of User
 		scanf("%d",&ch);
-		switch(ch)
+		switch(ch) //Checking the Choice with help or Switch Case
 		{
 			case 1:
 				printf("Enter the Value To Insert :- ");
