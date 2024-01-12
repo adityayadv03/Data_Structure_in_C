90e4ce75936d711af9d5912ce5b636c11073f53f
adityayadv03 <officialuser.aditya2022@gmail.com>
2024-01-12 19:31:18 +0530

    Queue Representation Linked List

--- Queue/Linked_List_Representation_Queue.c
+++ Queue/Linked_List_Representation_Queue.c
@@ -7,61 +7,61 @@ struct node //Basic Node To represent One Element Of Queue
 	struct node *next;
 };
 typedef struct node Node; //Define a shortname to struct node as Node
-Node *front=NULL;
+Node *front=NULL; //Initially the front is NULL and Queue is Empty
 Node *rear;
-void ins(int val)
+void ins(int val) //Function to Insert any Value in the Queue
 {
-	Node *newnode;
+	Node *newnode; //Creating a Newnode
 	newnode=(Node *)malloc(sizeof(Node));
 	newnode->data=val;
 	newnode->next=NULL;
-	if(front==NULL)
+	if(front==NULL) //If the front is NULL then newnode is the first element and position is stored to front and rear
 	{
 		front=newnode;
 		rear=newnode;
 	}
-	else
+	else //else newnode is added after rear
 	{
 		rear->next=newnode;
 		rear=newnode;
 	}
 }
-int del()
+int del() //Function to delete any element From Queue
 {
 	int val;
-	if(front==NULL)
+	if(front==NULL) //Checking whether the queue is empty or not if the front is NULl then Queue is Empty
 	{
 		printf("\nUnderflow . \n");
 		printf("\n");
 		val=-1;
 	}
-	else
+	else //Otherwise Deleting the element from front
 	{
 		val=front->data;
 		front=front->next;
 	}
 	return val;
 }
-void peek()
+void peek() //Function to check the first element of Queue
 {
-	if(front==NULL)
+	if(front==NULL) //Checking whether the queue is empty or not if the front is NULl then Queue is Empty
 	{
 		printf("\nQueue is Empty . ");
 		printf("\n");
 	}
-	else
+	else //otherwise printing the first element
 	{
 		printf("\nThe first Element is :- %d . \n",front->data);
 	}
 	printf("\n");
 }
-void display()
+void display() //Function to display all the element of Queue
 {
-	if(front==NULL)
+	if(front==NULL) //Checking whether the queue is empty or not if the front is NULl then Queue is Empty
 	{
 		printf("\nQueue is Empty . ");
 	}
-	else
+	else //otherwise printing all the elements
 	{
 		rear=front;
 		printf("\nElement of Queue :- ");
@@ -73,10 +73,10 @@ void display()
 	}
 	printf("\n\n");
 }
-int main()
+int main() //Main Function to Make a Menu Driven Program and calling the above functions Correctly
 {
 	int ch,value;
-	do
+	do //Using Do while Loop
 	{
 		printf("Welcome To Queue . \n");
 		printf("1.Insertion . \n");
@@ -84,9 +84,9 @@ int main()
 		printf("3.Peek . \n");
 		printf("4.Display . \n");
 		printf("5.Exit . \n");
-		printf("Enter Your Choice :- ");
+		printf("Enter Your Choice :- "); //Taking User Choice For the above menu
 		scanf("%d",&ch);
-		switch(ch)
+		switch(ch) //Using Switch Case to Call the Right Function
 		{
 			case 1:
 				printf("Enter the Value To Insert :- ");
