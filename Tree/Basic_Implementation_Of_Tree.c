97fe4ba9fde92043d0e53f58e21dd7513c6c34c3
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-01-23 19:31:50 +0530

    Tree Representation

--- Tree/Basic_Implementation_Of_Tree.c
+++ Tree/Basic_Implementation_Of_Tree.c
@@ -1,21 +1,22 @@
 /*Author :- Aditya Yadav */
 #include<stdio.h>
 #include<stdlib.h> //Program to Implement a Basic Tree Non-Linear Data Structure
-struct node{
-    int data;
-    struct node *left,*right;
-};
-typedef struct node Node;
-Node* createNode(int val)
+struct node //Creating a basic node used in a tree
 {
-    Node* newnode;
-    newnode=(Node*)malloc(sizeof(Node));
+    int data; //To Store the integer data of node
+    struct node *left,*right; //To store the child Node
+};
+typedef struct node Node; //Create a shortcut of struct node as Node
+Node* createNode(int val) //A function to create a node with a given integer value
+{
+    Node* newnode; //created a new node
+    newnode=(Node*)malloc(sizeof(Node)); //allocating the size using malloc
     newnode->data=val;
     newnode->left=NULL;
     newnode->right=NULL;
-    return newnode;
+    return newnode; //returning the created Node
 }
-void preOrder(Node *root)
+void preOrder(Node *root) //Recursive Function to print the preorder traversal
 {
     if(root!=NULL)
     {
@@ -24,7 +25,7 @@ void preOrder(Node *root)
         preOrder(root->right);
     }
 }
-void inOrder(Node *root)
+void inOrder(Node *root) //Recursive Function to print the Inorder traversal
 {
     if(root!=NULL)
     {
@@ -33,7 +34,7 @@ void inOrder(Node *root)
         inOrder(root->right);
     }
 }
-void postOrder(Node *root)
+void postOrder(Node *root) //Recursive Function to print the postorder traversal
 {
     if(root!=NULL)
     {
@@ -44,24 +45,21 @@ void postOrder(Node *root)
 }
 int main()
 {
-    int n;
-    printf("Enter the Value of Root :- ");
-    scanf("%d",&n);
-    Node* root=createNode(n);
-    Node* n1=createNode(4);
-    Node* n2=createNode(10);
-    Node* n3=createNode(2);
-    Node* n4=createNode(3);
-    root->left=n1;
-    root->right=n2;
-    n1->left=n3;
-    n3->right=n4;
-    printf("The Preorder Traversal is :- ");
+    Node* root=createNode(5); //Creating the root node with value 5
+    Node* n1=createNode(4); //Creating a node with value 4
+    Node* n2=createNode(10); //Creating a node with value 10
+    Node* n3=createNode(2); //Creating a node with value 2
+    Node* n4=createNode(3); //Creating a node with value 3
+    root->left=n1; //Connecting Node
+    root->right=n2; //Connecting Node
+    n1->left=n3; //Connecting Node
+    n3->right=n4; //Connecting Node
+    printf("The Preorder Traversal is :- "); //Printing the Preorder Traversal
     preOrder(root);
     printf("\n");
-    printf("The Inorder Traversal is :- ");
+    printf("The Inorder Traversal is :- "); //Printing the Inorder Traversal
     inOrder(root);
     printf("\n");
-    printf("The Postorder Traversal is :- ");
+    printf("The Postorder Traversal is :- "); //Printing the Postorder Traversal
     postOrder(root);
 }
