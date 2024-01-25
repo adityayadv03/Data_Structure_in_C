e440c9782da479121664d2cae05666b0a1ea6250
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-01-25 22:39:25 +0530

    Adding Some Required Comment

--- Tree/Binary_Search_Tree.c
+++ Tree/Binary_Search_Tree.c
@@ -1,45 +1,46 @@
+/*Author :- Aditya Yadav */
 #include<stdio.h>
-#include<stdlib.h>
-struct node
+#include<stdlib.h> //Program to Implement the Binary Search Tree
+struct node //Creating a basic node used in a tree
 {
-    int data;
-    struct node *left,*right;
+    int data; //To Store the integer data of node
+    struct node *left,*right; //To store the child Node
 };
-typedef struct node Node;
-void createBST(Node* root,int val)
+typedef struct node Node; //Create a shortcut of struct node as Node
+void createBST(Node* root,int val) //Function to insert a Newnode in the Binary Search Tree 
 {
     Node* prev=NULL;
-    while(root!=NULL)
+    while(root!=NULL) //Traversing till we find the last Null Node
     {
-        prev=root;
-        if(root->data==val)
+        prev=root; //Storing the previous node address
+        if(root->data==val) //Checking if the Value we want to insert is already present we will not insert it
         {
             return;
         }
-        else if(val<root->data)
+        else if(val<root->data) //If the Value we want to insert in smaller than the current node value we will go to left subtree
         {
             root=root->left;
         }
-        else
+        else //If the Value we want to insert in greater than the current node value we will go to Right subtree
         {
             root=root->right;
         }
-    }
+    } //After finding the right place to insert the node
     Node* newnode;
-    newnode=(Node*)malloc(sizeof(Node));
-    newnode->data=val;
+    newnode=(Node*)malloc(sizeof(Node)); //Creating the Newnode and allocating the memory
+    newnode->data=val; //Inserting the data to the Newnode
     newnode->left=NULL;
     newnode->right=NULL;
-    if(val<prev->data)
+    if(val<prev->data) //If the val is less than the prev node data then inserting the newnode in the left
     {
         prev->left=newnode;
     }
-    else
+    else //Otherwise inserting the value in the right
     {
         prev->right=newnode;
     }
 }
-void preOrder(Node *root)
+void preOrder(Node *root) //Recursive Function to print the preorder traversal
 {
     if(root!=NULL)
     {
@@ -48,7 +49,7 @@ void preOrder(Node *root)
         preOrder(root->right);
     }
 }
-void inOrder(Node *root)
+void inOrder(Node *root) //Recursive Function to print the Inorder traversal
 {
     if(root!=NULL)
     {
@@ -57,7 +58,7 @@ void inOrder(Node *root)
         inOrder(root->right);
     }
 }
-void postOrder(Node *root)
+void postOrder(Node *root) //Recursive Function to print the postorder traversal
 {
     if(root!=NULL)
     {
@@ -68,28 +69,28 @@ void postOrder(Node *root)
 }
 int main()
 {
-    Node* root;
-    root=(Node*)malloc(sizeof(Node));
+    Node* root; //Declaring the root Node
+    root=(Node*)malloc(sizeof(Node)); //Allocating the Memory to root
     root->left=NULL;
     root->right=NULL;
     int n,ele;
-    printf("Enter The Number of Vertices :- ");
+    printf("Enter The Number of Vertices :- "); //Taking the Number of Vertices in tree
     scanf("%d",&n);
-    printf("Enter the Value of Root Node :- ");
+    printf("Enter the Value of Root Node :- "); //Taking the Value of root Node
     scanf("%d",&root->data);
     for(int i=1 ; i<n ; i++)
     {
-        printf("Enter the Value of Vertices :- ");
+        printf("Enter the Value of Vertices :- "); //Taking the Rest of Remaining Root
         scanf("%d",&ele);
         createBST(root,ele);
     }
-    printf("The Preorder Traversal :- ");
+    printf("The Preorder Traversal :- "); //Printing the Preorder Traversal
     preOrder(root);
     printf("\n");
-    printf("The Inorder Traversal :- ");
+    printf("The Inorder Traversal :- "); //Printing the Inorder Traversal
     inOrder(root);
     printf("\n");
-    printf("The Postorder Traversal :- ");
+    printf("The Postorder Traversal :- "); //Printing the Postorder Traversal
     postOrder(root);
     printf("\n");
 }
